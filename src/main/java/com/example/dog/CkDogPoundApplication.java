d2017bc371823de689e0407fd553f71ed24a2eb0
Conor Kelly <conorkelly10@aol.com>
2022-03-03 19:13:03 +0000

    creating spring project

---
+++ src/main/java/com/example/dog/CkDogPoundApplication.java
@@ -0,0 +1,13 @@
+package com.example.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkDogPoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkDogPoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dog/CkDogPoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkDogPoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
