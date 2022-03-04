d5e05767e49477eb41cc10c2868bce47070a2a22
Conor Kelly <conorkelly10@aol.com>
2022-03-04 09:49:19 +0000

    importing service class

--- src/main/java/com/example/dog/web/DogController.java
+++ src/main/java/com/example/dog/web/DogController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dog.domain.Dog;
+import com.example.dog.service.DogService;
 
 
-
+@RestController
 public class DogController {
 
 private DogService service;
