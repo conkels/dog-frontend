3417f99154f4675cb9add1eb1eb4e3936227eebd
Conor Kelly <conorkelly10@aol.com>
2022-03-08 12:20:08 +0000

    creating html and js files with getAll and create functionality

---
+++ dog.js
@@ -0,0 +1,86 @@
+let getData = async () => {
+  let response = await fetch("http://localhost:8080/getAll");
+  if (response.status !== 200) {
+    throw new Error("Request has failed!");
+  }
+  console.log("Request Successful");
+  let jsonData = await response.json();
+  return jsonData;
+  // return jsonData.data;
+};
+
+const paragraphToSelect = document.querySelector("#dataPara");
+
+let showData = async () => {
+  let returnedData = await getData();
+  paragraphToSelect.innerHTML = "";
+  for (let d of returnedData) {
+    let div = document.createElement("div");
+    div.style = "margin:10px;";
+    div.innerHTML = `Breed: ${d.breed}, Name: ${d.name}, Age: ${d.age}, Male: ${d.isMale}`;
+    paragraphToSelect.append(div);
+  }
+};
+
+let clearData = () => (paragraphToSelect.innerHTML = "No data shown");
+
+function inputForm() {
+  if (cbtn.value == "Create") {
+    form1.style.visibility = "visible";
+    cbtn.value = "Hide";
+  } else {
+    form1.style.visibility = "hidden";
+    cbtn.value = "Create";
+  }
+}
+
+let getOne = async (id) => {
+  let response = await fetch("http://localhost:8080/get/id");
+  if (response.status !== 200) {
+    throw new Error("Request has failed!");
+  }
+  console.log("Request Successful");
+  let jsonData = await response.json();
+  return jsonData;
+  // return jsonData.data;
+};
+
+let showOne = async () => {
+  let returnedData = await getOne();
+  paragraphToSelect.innerHTML = "";
+
+  let div = document.createElement("div");
+  div.style = "margin:10px;";
+  div.innerHTML = `Breed: ${returnedData.breed}, Name: ${returnedData.name}, Age: ${returnedData.age}, Male: ${returnedData.isMale}`;
+  paragraphToSelect.append(div);
+};
+
+function showFindSearch() {
+  if (fbtn.value == "Find") {
+    find1.style.visibility = "visible";
+    fbtn.value = "Hide";
+  } else {
+    find1.style.visibility = "hidden";
+    fbtn.value = "Find";
+  }
+}
+
+let createOne = async() => {
+fetch("http://localhost:8080/create", { 
+    method: 'post', 
+    headers: {
+      "Content-type": "application/json" 
+    },
+    body: JSON.stringify( 
+      {
+        "breed": breedTab.value,
+        "name": nameTab.value,
+        "age": ageTab.value,
+        "isMale": genderTab.value
+      }
+    )
+  })
+  .then(res => res.json())
+  .then((data) => console.log(`Request succeeded with JSON response ${data}`))
+  .catch((error) => console.log(`Request failed ${error}`));
+}
