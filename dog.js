bfbd8b247957bd934482f8c5b0acb02037f8c9e7
Conor Kelly <conorkelly10@aol.com>
2022-03-10 19:29:39 +0000

    Improving user experience

--- dog.js
+++ dog.js
@@ -28,17 +28,17 @@ function inputForm() {
   if (cbtn.value == "Create") {
     form1.style.visibility = "visible";
     cbtn.value = "Hide";
-    abtn.style.visibility="visible"
+    abtn.style.visibility = "visible";
   } else {
     form1.style.visibility = "hidden";
     cbtn.value = "Create";
-    abtn.style.visibility="hidden"
+    abtn.style.visibility = "hidden";
   }
 }
 
 let getOne = async (id) => {
-  let d=findSearch.value
-  let response = await fetch("http://localhost:8080/get/"+d);
+  let d = findSearch.value;
+  let response = await fetch("http://localhost:8080/get/" + d);
   if (response.status !== 200) {
     throw new Error("Request has failed!");
   }
@@ -56,6 +56,7 @@ let showOne = async () => {
   div.style = "margin:10px;";
   div.innerHTML = `ID: ${returnedData.id}, Breed: ${returnedData.breed}, Name: ${returnedData.name}, Age: ${returnedData.age}, Male: ${returnedData.isMale}`;
   paragraphToSelect.append(div);
+  findSearch.value=""
 };
 
 function showFindSearch() {
@@ -68,25 +69,28 @@ function showFindSearch() {
   }
 }
 
-let createOne = async() => {
-fetch("http://localhost:8080/create", { 
-    method: 'post', 
+let createOne = async () => {
+  fetch("http://localhost:8080/create", {
+    method: "post",
     headers: {
-      "Content-type": "application/json" 
+      "Content-type": "application/json",
     },
-    body: JSON.stringify( 
-      {
-        "breed": breedTab.value,
-        "name": nameTab.value,
-        "age": ageTab.value,
-        "isMale": genderTab.value
-      }
-    )
+    body: JSON.stringify({
+      breed: breedTab.value,
+      name: nameTab.value,
+      age: ageTab.value,
+      isMale: genderTab.value,
+    }),
   })
-  .then(res => res.json())
-  .then((data) => console.log(`Request succeeded with JSON response ${data}`))
-  .catch((error) => console.log(`Request failed ${error}`));
-}
+    .then((res) => res.json())
+    .then((data) => console.log(`Request succeeded with JSON response ${data}`))
+    .catch((error) => console.log(`Request failed ${error}`));
+
+  breedTab.value = "";
+  nameTab.value = "";
+  ageTab.value = "";
+  genderTab.value = "";
+};
 
 function showDelete() {
   if (dbtn.value == "Delete") {
@@ -98,66 +102,64 @@ function showDelete() {
   }
 }
 
-let deleteOne = async() => {
-let id = deleteInput.value
-      fetch("http://localhost:8080/remove/"+id, { 
-          method: 'delete' 
-        })
-        .then((data) => {
-          console.log(`Request succeeded with JSON response ${data}`);
-          // some function to execute if successful
-        })
-        .catch((error) => {
-          console.log(`Request failed ${error}`)
-        });
-      }
+let deleteOne = async () => {
+  let id = deleteInput.value;
+  fetch("http://localhost:8080/remove/" + id, {
+    method: "delete",
+  })
+    .then((data) => {
+      console.log(`Request succeeded with JSON response ${data}`);
+      // some function to execute if successful
+    })
+    .catch((error) => {
+      console.log(`Request failed ${error}`);
+    });
+    deleteInput.value=""
+};
 
-      function showReplace() {
-        if (rbtn.value == "Replace") {
-          replace1.style.visibility = "visible";
-          rbtn.value = "Hide";
-        } else {
-          replace1.style.visibility = "hidden";
-          form1.style.visibility="hidden"
-          ebtn.style.visibility="hidden"
-          rbtn.value = "Replace";
-        }
-      }
+function showReplace() {
+  if (rbtn.value == "Replace") {
+    replace1.style.visibility = "visible";
+    rbtn.value = "Hide";
+  } else {
+    replace1.style.visibility = "hidden";
+    form1.style.visibility = "hidden";
+    ebtn.style.visibility = "hidden";
+    rbtn.value = "Replace";
+  }
+}
 
-      let replaceOne = async() => {
-        let id=replaceSearch.value
-        fetch("http://localhost:8080/replace/"+id, { 
-            method: 'put', 
-            headers: {
-              "Content-type": "application/json" 
-            },
-            body: JSON.stringify( 
-              {
-                "breed": breedTab.value,
-                "name": nameTab.value,
-                "age": ageTab.value,
-                "isMale": genderTab.value
-              }
-            )
-          })
-          .then(res => res.json())
-          .then((data) => console.log(`Request succeeded with JSON response ${data}`))
-          .catch((error) => console.log(`Request failed ${error}`));
+let replaceOne = async () => {
+  let id = replaceSearch.value;
+  fetch("http://localhost:8080/replace/" + id, {
+    method: "put",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      breed: breedTab.value,
+      name: nameTab.value,
+      age: ageTab.value,
+      isMale: genderTab.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(`Request succeeded with JSON response ${data}`))
+    .catch((error) => console.log(`Request failed ${error}`));
 
-          breedTab.value=""
-          nameTab.value=""
-          ageTab.value=""
-          genderTab.value=""
+  breedTab.value = "";
+  nameTab.value = "";
+  ageTab.value = "";
+  genderTab.value = "";
+};
 
-        }
-
-        function replaceForm() {
-          if (okbtn.value == "OK") {
-            form1.style.visibility = "visible";
-            ebtn.style.visibility="visible"
-            abtn.style.visibility="hidden"
-          } else {
-            form1.style.visibility = "hidden";
-            abtn.style.visibility="visible"
-          }
-        }
+function replaceForm() {
+  if (okbtn.value == "OK") {
+    form1.style.visibility = "visible";
+    ebtn.style.visibility = "visible";
+    abtn.style.visibility = "hidden";
+  } else {
+    form1.style.visibility = "hidden";
+    abtn.style.visibility = "visible";
+  }
+}
